1c18aa1f896e535caddb813854f2dfef9b521ab4
jsf <1195143136@qq.com>
2022-06-10 17:55:13 +0800

    修改：测试log4j2日志

---
+++ springboot-demo-test/src/main/java/com/rx/SpringbootDemoTestApplication.java
@@ -0,0 +1,14 @@
+package com.rx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoTestApplication.class, args);
+
+    }
+
+}
