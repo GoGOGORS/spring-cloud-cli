3260dbd04a9ae23fcce8782e8eec2fc60dec8c87
jsf <1195143136@qq.com>
2022-10-21 18:07:05 +0800

    修改：无用提交内容

---
+++ springboot-demo-test/src/main/java/com/rx/auth/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.rx.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
+/**
+ * {@code @author: jsf}
+ * {{@code @Date} @date: 2022-10-15 14:56}
+ * {{@code @Description} @Description: }
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry filterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic().and().authorizeHttpRequests()
+                .anyRequest().authenticated();
+    }
+
+
+
+}
